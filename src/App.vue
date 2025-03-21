43ccb76e6a8750f9f002cdf361fc7a13449d4134
Brian La <133820463+bla29@users.noreply.github.com>
2025-03-20 20:59:04 -0700

    modal finished

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import Modal from './components/Modal.vue'
+import {ref} from 'vue'
+
+let modal = ref(false)
+
+const toggleModal = () => {
+  modal.value = !modal.value
+  console.log (modal.value)
+}
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <img src="@/assets/bass.png" width="200" height="200" />
+      <div>
+        <h1 class="green">Fishy-Dex</h1>
+        <h3>
+          A collection of your fish catches.
+        </h3>
+      </div>
+    </div>
+    <button class = "button" @click = "toggleModal()">Log your catch!</button>
+    <div v-if = "modal">
+      <Modal @close = "toggleModal()" />
+    </div>
+    </header>
+
+  <body>
+    <li>
+      hi
+    </li>
+  </body>
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  text-align: center;
+}
+
+.button {
+  margin: 50px;
+  padding: 10px;
+}
+
+.container {
+  align-self: center;
+}
+
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class = "backdrop" @click ="closeModal()">
+        <div class = "modal">
+            <h2>Enter your fish details</h2>
+            <p>Species:</p>
+            <input type = "text">
+            <p>Weight(lbs):</p>
+            <input type = "text">
+            <p>Date(MM/DD/YYYY):</p>
+            <input type = "text">
+            <br>
+            <button>Confirm</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {defineEmits} from "vue"
+
+const emit = defineEmits('close')
+
+const closeModal = () => {
+    emit('close')
+}
+
+</script>
+
+<style scoped>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+</style>
